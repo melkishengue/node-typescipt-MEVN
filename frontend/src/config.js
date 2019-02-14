d69953d5054c80716b8b0ce433365f4097904589
Melkis Hengue <henguemelkispro@yahoo.com>
2019-02-14 19:31:27 +0100

    Changed base url for movie service

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,5 @@
-let server = `${window.location.protocol}//${window.location.hostname}:8080`;
-console.log('server', server);
+export const backendUri = `${window.location.protocol}//${window.location.hostname}:8080`;
 
-export const backendUri = server;
+export default {
+    backendUri
+}
